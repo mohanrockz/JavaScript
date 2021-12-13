6e21b56d266b1bcef93a31bb6dd5ccc1bd0f10a8
mohanrockz <mohanpujari1999@gmail.com>
2021-12-13 18:50:39 +0530

    js-assignment updated

---
+++ js-assignment/src/Components/Cart/Cart.styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.aside`
+  font-family: Arial, Helvetica, sans-serif;
+  width: 600px;
+  padding: 20px;
+  border: 5px solid #fb641b;
+  
+
+#total{
+  font-size:san-serif;
+  text-align:center;
+  background-color:orange;
+} 
+
+#headings{
+  font-size:san-serif;
+  text-align:center;
+  background-color:orange;
+
+}
+CartItem{
+    background-color:orange;
+}
+
+  cartborder{
+    height: 100%;
+    border: 1px solid black;
+  }
+
+  .button1{
+    width:100px;
+    margin: 0 auto;
+    display: block;
+    background-color: #0000FF;
+    border-radius: 5px;
+    padding: 8px;
+    color: #fff;
+   
+  }
+  .button1:hover{
+    background-color:green;
+  }
+
+  button{
+    border:1px solid black;
+    border-radius:50%;
+    font-size:30px;
+    
+  }
+  button:hover{
+    opacity:110%;
+    background-color:white;
+  }
+`;
